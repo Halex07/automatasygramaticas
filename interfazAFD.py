a92c4661e15f7942e7ae6b77becbe4bf14c13e76
Halex07 <92344208+Halex07@users.noreply.github.com>
2022-12-18 00:01:53 -0600

    Add files via upload

--- interfazAFD.py
+++ interfazAFD.py
@@ -7,11 +7,11 @@ from tkinter import messagebox
 from automatas import *
 from gramaticas import *
 from ayudaafd import *
-import functools 
+import functools
 
 class bienvenida2():
     def __init__(self):
-        self.win2 = Tk()
+        self.win2 = tk.Toplevel()
         self.win2.resizable(True,False) # Redimensionar la ventana
         self.win2.title('..::ADF::..') # Titulo de la ventana
         self.win2.geometry('1000x700') # Tamaño de la ventana
@@ -34,65 +34,50 @@ class bienvenida2():
         self.cont1 = Frame(height=1000, width=700) # Se coloca sobre la ventana
         self.cont1.config(bg='#37474f')
         self.cont1.pack(padx=10, pady=10)
-        Label(self.cont1, text="...:::...Creación de Automatas...:::...", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=200, y=20)
-        Label(self.cont1, text="Ingrese el nombre del automata:", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=50)
-        Label(self.cont1, text="Ingrese los estados del automata separados por una coma: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=80)
-        Label(self.cont1, text="Ingrese el alfabeto del automata separados por una coma: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=110)
-        Label(self.cont1, text="Ingrese el estado inicial del automata: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=140)
-        Label(self.cont1, text="Ingrese los estados de aceptacion del automata separados por una coma: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=170)
-        Label(self.cont1, text="Ingrese las transiciones del automata separados por una coma: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=200)
+        Label(self.win2, text="...:::...Creación de Automatas...:::...", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=200, y=20)
+        Label(self.win2, text="Ingrese el nombre del automata:", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=50)
+        Label(self.win2, text="Ingrese los estados del automata separados por una coma: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=80)
+        Label(self.win2, text="Ingrese el alfabeto del automata separados por una coma: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=110)
+        Label(self.win2, text="Ingrese el estado inicial del automata: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=140)
+        Label(self.win2, text="Ingrese los estados de aceptacion del automata separados por una coma: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=170)
+        Label(self.win2, text="Ingrese las transiciones del automata separados por una coma: ", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=52).place(x=90, y=200)
         
         
 
         self.entrada1 = StringVar()
-        Entry(self.cont1, textvariable=self.entrada1, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=50)
+        Entry(self.win2, textvariable=self.entrada1, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=50)
         self.content2 = StringVar()
-        Entry(self.cont1, textvariable=self.content2, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=80)
+        Entry(self.win2, textvariable=self.content2, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=80)
         self.content3 = StringVar()
-        Entry(self.cont1, textvariable=self.content3,font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=110)
+        Entry(self.win2, textvariable=self.content3,font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=110)
         self.content4 = StringVar()
-        Entry(self.cont1, textvariable=self.content4,font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=140)
+        Entry(self.win2, textvariable=self.content4,font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=140)
         self.content5 = StringVar()
-        Entry(self.cont1, textvariable=self.content5,font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=170)
+        Entry(self.win2, textvariable=self.content5,font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=170)
         self.content6 = StringVar()
-        Entry(self.cont1, textvariable=self.content6,font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=200)
+        Entry(self.win2, textvariable=self.content6,font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=720, y=200)
 
        
-        Button(self.cont1, text="Calcular",command=self.calc, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=15).place(x=420, y=280)
-        Button(self.cont1, text="Cargar Archivo",command=helpafd, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=15).place(x=420, y=320)
-        Button(self.cont1, text="Regresar",command=lambda:self.win2.destroy(), font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=15).place(x=420, y=360)
+        Button(self.win2, text="Calcular",command=self.calc, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=15).place(x=420, y=280)
+        Button(self.win2, text="Ayuda",command=helpafd, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=15).place(x=420, y=320)
+        Button(self.win2, text="Regresar",command=lambda:self.win2.destroy(), font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=15).place(x=420, y=360)
 
 
         
-        self.win2.mainloop(self.cont1.mainloop())
         
-
-        
-    
-        
-        
-
-    
-
-        
-       
-
-        
-        
-
-        
+           
     def calc (self):
 
         n1=self.entrada1.get()
         print(n1)
         
          #labels
-        Label(self.frame, text=self.entrada1.get(), font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=250, y=440)
-        Label(self.frame, text="", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=470)
-        Label(self.frame, text="", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=500)
-        Label(self.frame, text="", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=530)
-        Label(self.frame, text="self.txt5", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=560)
-        Label(self.frame, text="self.txt6", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=590)
+        Label(self.win2, text=self.entrada1.get(), font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=20).place(x=250, y=440)
+        Label(self.win2, text="", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=470)
+        Label(self.win2, text="", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=500)
+        Label(self.win2, text="", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=530)
+        Label(self.win2, text="self.txt5", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=560)
+        Label(self.win2, text="self.txt6", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=590)
 
 
 
@@ -176,7 +161,10 @@ class bienvenida2():
         print("Automatas ingresados: ")
         for automata in lista_automatas:
             fin=print(automata.__str__())
-            Label(self.win2, text=self.txt6, font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=620)
+            Label(self.win2, text="self.txt6", font=('Times New Roman',15), fg='#000000', bg='#ff6f00', width=50).place(x=75, y=620)
+
+
+
 
 
 
